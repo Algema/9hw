5d0a2452409f1e1a1cad67b7c0af2418625c584e
anastasiazheltikova <znastya2905@icloud.com>
2022-06-05 21:37:19 +0700

    дз

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Author authorR= new Author("J.K","Rowling");
+        Book book1 = new Book(" Harry Potter and the philosopher's stone",1997, authorR);
+        System.out.println(authorR.getName()+ " "+authorR.getSurname()+book1.getNameBook()+" "+book1.getPublicationYear());
+        Author authorC = new Author("C.S","Lewis");
+        Book book2 = new Book("The chronicles of Narnia",1950,authorC);
+        System.out.println( authorC.getName()+" "+authorC.getSurname()+ book2.getNameBook()+" "+book2.getPublicationYear());
+        book1.setPublicationYear(2000);
+        System.out.println(book1.getPublicationYear());
+
+
+
     }
 }
